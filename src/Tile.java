b782dc3921a6a76ab57ab1df3110a8b9f1c84c4f
Anthony Cobb <anthonycobb@protonmail.com>
2020-11-08 20:15:37 -0500

    removed unused code

--- src/Tile.java
+++ src/Tile.java
@@ -16,33 +16,9 @@ public class Tile
         return this.x;
     }
 
-    public void setX(int x)
-    {
-        if (x < 8 && x >= 0)
-        {
-            this.x = x;
-        }
-        else
-        {
-            throw new IllegalArgumentException();
-        }
-    }
-
     public int getY()
     {
         return this.y;
-    }
-
-    public void setY(int y)
-    {
-        if (y < 8 && y >= 0)
-        {
-            this.y = y;
-        }
-        else
-        {
-            throw new IllegalArgumentException();
-        }
     }
 
     public Piece getPiece()
