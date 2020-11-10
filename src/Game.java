fb69594cb9a706a743e7f11f1d6df52057a61757
Anthony Cobb <anthonycobb@protonmail.com>
2020-11-10 04:04:34 -0500

    added graphics

--- src/Game.java
+++ src/Game.java
@@ -1,69 +1,130 @@
-import Chess.Board;
+import Chess.Tile;
 import Chess.ChessGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Line2D;
 import java.util.Scanner;
 
-public class Game
+public class Game extends JPanel
 {
-    private static Scanner s = new Scanner(System.in);
+    private ChessGame chess = new ChessGame();
+    private Scanner s = new Scanner(System.in);
+    private int previousMouseX;
+    private int previousMouseY;
+    private int mouseX;
+    private int mouseY;
 
-    public static void main(String[] args)
+    public Game()
     {
-        Board newGame = new Board(8, 8);
-        printBoard(newGame);
+        getMouseInput();
     }
 
-    private static void printBoard(Board board)
+    // note: I could also implement mouse inputs
+    private void getMouseInput()
     {
-        ChessGame c = new ChessGame();
-        c.board = board;
-        while(true)
+        addMouseListener(new MouseAdapter()
         {
-            System.out.print("   ");
-            for (int x = -1; x < 8; x++)
+            @Override
+            public void mousePressed(MouseEvent e)
             {
+                super.mousePressed(e);
 
-                for (int y = 0; y < 8; y++)
+                previousMouseX = mouseX;
+                previousMouseY = mouseY;
+
+                mouseX = e.getX() / 100;
+                mouseY = e.getY() / 100;
+
+                System.out.printf("%d, %d\n", mouseX, mouseY);
+                movePiece();
+                repaint();
+            }
+        });
+    }
+
+    private void movePiece()
+    {
+        if (chess.getTile(previousMouseX, previousMouseY).getPiece() != null)
+        {
+            if (chess.isTurnWhite() == chess.getTile(previousMouseX, previousMouseY).getPiece().isWhite())
+            {
+                chess.movePiece(previousMouseX, previousMouseY, mouseX, mouseY);
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        //Graphics g2d = (Graphics2D) g;
+
+        drawBoard(g);
+        fillValidMoves(g);
+
+        g.drawString("S", 50, 50);
+    }
+
+    private void drawBoard(Graphics g)
+    {
+        for (int x = 0; x < chess.getBoardSizeX(); x++)
+        {
+            for (int y = 0; y < chess.getBoardSizeY(); y++)
+            {
+                g.drawRect(x * 100, y * 100, 100, 100);
+                if (chess.getTile(x, y).getPiece() != null)
                 {
-                    if (y == 0 && x != -1)
+                    if (chess.getTile(x, y).getPiece().isWhite())
                     {
-                        System.out.printf("|%c|", x + 97);
-                    }
-                    if (x == -1)
-                    {
-                        System.out.printf("|%2.0f|", (float)(y + 1));
+                        g.setColor(Color.gray);
+                        g.fillOval((x * 100) + 25, (y * 100) + 25, 50, 50);
+                        g.setColor(Color.black);
                     }
                     else
                     {
-                        if (board.getTile(x, y).getPiece() != null)
+                        g.fillOval((x * 100) + 25, (y * 100) + 25, 50, 50);
+                    }
+                }
+            }
+        }
+    }
+
+    private void fillValidMoves(Graphics g)
+    {
+        if (chess.getTile(mouseX, mouseY).getPiece() != null)
+        {
+            if (chess.isTurnWhite() == chess.getTile(mouseX, mouseY).getPiece().isWhite())
+            {
+                for (int x = 0; x < chess.getBoardSizeX(); x++)
+                {
+                    for (int y = 0; y < chess.getBoardSizeY(); y++)
+                    {
+                        if (chess.isMoveLegal(mouseX, mouseY, x, y))
                         {
-                            System.out.printf(" %c%c ", board.getTile(x, y).getPiece().isWhite() ? 'W' : 'B', board.getTile(x, y).getPiece().getId());
-                        }
-                        else
-                        {
-                            System.out.printf(" XX ");
+                            g.setColor(Color.red);
+                            g.drawOval((x * 100) + 25, (y * 100) + 25, 50, 50);
+                            g.setColor(Color.black);
                         }
                     }
                 }
-                System.out.println("");
-            }
-
-            while(true)
-            {
-                System.out.println(c.isKingInCheck());
-                System.out.println(c.isKingInCheckmate());
-
-                System.out.print("Select Piece: ");
-                String pieceFrom = s.nextLine();
-
-                System.out.print("Move Piece To: ");
-                String pieceTo = s.nextLine();
-
-                c.movePiece(pieceFrom, pieceTo);
-                break;
             }
         }
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame jf = new JFrame();
+        jf.setSize(800, 800);
+        jf.setResizable(false);
+        jf.setTitle("Chess");
+        // sets location on screen when it opens
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
+        // adds all components and listeners to the object
+        jf.getContentPane().add(new Game());
+        // note: this needs to be called last for everything to load properly
+        jf.setVisible(true);
     }
 }
