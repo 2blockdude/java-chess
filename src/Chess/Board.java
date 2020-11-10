e2fec6963dd3f9de0ed215f00ff1c791487abb41
Anthony Cobb <anthonycobb@protonmail.com>
2020-11-10 03:59:36 -0500

    changed piece values

--- src/Chess/Board.java
+++ src/Chess/Board.java
@@ -5,6 +5,12 @@ public class Board
     private int boardSizeX;
     private int boardSizeY;
     private Tile[][] board;
+    private final int pawnValue = 1;
+    private final int horseValue = 3;
+    private final int bishopValue = 3;
+    private final int rookValue = 5;
+    private final int queenValue = 9;
+    private final int kingValue = 900;
 
     public Board(int boardSizeX, int boardSizeY)
     {
@@ -46,39 +52,39 @@ public class Board
 
         // SET BOARD
         // white
-//        board[0][1].setPiece(new Pawn(true, 10));
-//        board[1][1].setPiece(new Pawn(true, 10));
-//        board[2][1].setPiece(new Pawn(true, 10));
-//        board[3][1].setPiece(new Pawn(true, 10));
-//        board[4][1].setPiece(new Pawn(true, 10));
-//        board[5][1].setPiece(new Pawn(true, 10));
-//        board[6][1].setPiece(new Pawn(true, 10));
-//        board[7][1].setPiece(new Pawn(true, 10));
-//        board[0][0].setPiece(new Rook(true, 10));
-//        board[1][0].setPiece(new Horse(true, 10));
-//        board[2][0].setPiece(new Bishop(true, 10));
-//        board[3][0].setPiece(new Queen(true, 10));
-//        board[4][0].setPiece(new King(true, 10));
-//        board[5][0].setPiece(new Bishop(true, 10));
-//        board[6][0].setPiece(new Horse(true, 10));
-//        board[7][0].setPiece(new Rook(true, 10));
+        board[0][1].setPiece(new Pawn(true, pawnValue));
+        board[1][1].setPiece(new Pawn(true, pawnValue));
+        board[2][1].setPiece(new Pawn(true, pawnValue));
+        board[3][1].setPiece(new Pawn(true, pawnValue));
+        board[4][1].setPiece(new Pawn(true, pawnValue));
+        board[5][1].setPiece(new Pawn(true, pawnValue));
+        board[6][1].setPiece(new Pawn(true, pawnValue));
+        board[7][1].setPiece(new Pawn(true, pawnValue));
+        board[0][0].setPiece(new Rook(true, pawnValue));
+        board[1][0].setPiece(new Horse(true, horseValue));
+        board[2][0].setPiece(new Bishop(true, bishopValue));
+        board[3][0].setPiece(new Queen(true, rookValue));
+        board[4][0].setPiece(new King(true, kingValue));
+        board[5][0].setPiece(new Bishop(true, bishopValue));
+        board[6][0].setPiece(new Horse(true, horseValue));
+        board[7][0].setPiece(new Rook(true, rookValue));
         // black
-//        board[0][6].setPiece(new Pawn(false, 10));
-//        board[1][6].setPiece(new Pawn(false, 10));
-//        board[2][6].setPiece(new Pawn(false, 10));
-//        board[3][6].setPiece(new Pawn(false, 10));
-//        board[4][6].setPiece(new Pawn(false, 10));
-//        board[5][6].setPiece(new Pawn(false, 10));
-//        board[6][6].setPiece(new Pawn(false, 10));
-//        board[7][6].setPiece(new Pawn(false, 10));
-//        board[0][7].setPiece(new Rook(false, 10));
-//        board[1][7].setPiece(new Horse(false, 10));
-//        board[2][7].setPiece(new Bishop(false, 10));
-//        board[3][7].setPiece(new Queen(false, 10));
-//        board[4][7].setPiece(new King(false, 10));
-//        board[5][7].setPiece(new Bishop(false, 10));
-//        board[6][7].setPiece(new Horse(false, 10));
-//        board[7][7].setPiece(new Rook(false, 10));
+        board[0][6].setPiece(new Pawn(false, pawnValue));
+        board[1][6].setPiece(new Pawn(false, pawnValue));
+        board[2][6].setPiece(new Pawn(false, pawnValue));
+        board[3][6].setPiece(new Pawn(false, pawnValue));
+        board[4][6].setPiece(new Pawn(false, pawnValue));
+        board[5][6].setPiece(new Pawn(false, pawnValue));
+        board[6][6].setPiece(new Pawn(false, pawnValue));
+        board[7][6].setPiece(new Pawn(false, pawnValue));
+        board[0][7].setPiece(new Rook(false, pawnValue));
+        board[1][7].setPiece(new Horse(false, horseValue));
+        board[2][7].setPiece(new Bishop(false, bishopValue));
+        board[3][7].setPiece(new Queen(false, rookValue));
+        board[4][7].setPiece(new King(false, kingValue));
+        board[5][7].setPiece(new Bishop(false, bishopValue));
+        board[6][7].setPiece(new Horse(false, horseValue));
+        board[7][7].setPiece(new Rook(false, rookValue));
 
 
         // Pawn test
@@ -122,12 +128,12 @@ public class Board
 
         // King test
         // castle
-        board[4][0].setPiece((new King(true, 900)));
+//        board[4][0].setPiece((new King(true, 900)));
 //        board[5][2].setPiece(new Horse(false, 10));
-        board[4][7].setPiece(new Rook(false, 10));
-        board[0][1].setPiece(new Rook(true, 10));
+//        board[4][7].setPiece(new Rook(false, 10));
+//        board[0][1].setPiece(new Rook(true, 10));
 //        board[0][3].getPiece().increaseMoves();
-        board[4][1].setPiece((new Queen(false, 20)));
+//        board[4][1].setPiece((new Queen(false, 20)));
 //        board[4][5].setPiece((new Queen(true, 30)));
 //        board[7][7].setPiece(new King(false, 10));
 //        board[2][1].setPiece(new Pawn(true, 10));
